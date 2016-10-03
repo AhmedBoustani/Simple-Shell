616ff62e0dc806cf6e8a3323bd251eb3e8bac0bb
Ahmed Amine Boustani <ahmed.a.boustani@gmail.com>
2016-10-03 16:14:33 +0100

    Simple Shell Updated

--- shell.c
+++ shell.c
@@ -9,27 +9,35 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define SIGKILL 9
+#define SIGKILL 9  //Signal to kil a process
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
+// Print the current directory
 void Print_Curr_Dir () {
-  char based[100];
-  getcwd(based, sizeof(based));
-  printf(":%s $ ", based);
-  
+  char dirpath[100]; // variable to store the path to the working directory
+  // get the path of the current working directory and store it in based
+  getcwd(dirpath, sizeof(dirpath));
+  // Print the result
+  printf(":" ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET "$ ", dirpath);
 }
 
-void getcomm(char *command){
+void Get_Command_Path(char *command){
   int status;
   pid_t pid;
+
   pid = fork();
 
   if (pid != 0) waitpid(-1, &status, 0);
 
   else {
+    // redirect the default output from stdout to the specified file: dir.txt
     freopen("dir.txt", "w", stdout);
     execl("/usr/bin/which", "which", command, NULL);
     fclose(stdout);
   }
+  // kill the process if it's not over
   kill(pid, SIGKILL);
 }
 
@@ -39,36 +47,46 @@ int main (int argc, char** argv){
   int status;
 
   while(1) {
+    // Print the path to the working directory
     Print_Curr_Dir();
+    // get the input from stdin and store it in variable input
     fgets(input, 100, stdin);
+    // tokenize the string in input into command string and argument string
     command = strtok_r(input, " \n", &saveptr);
     arg = strtok_r(NULL, " \n", &saveptr);
 
+    // stop the program if exit
     if (strcmp(command, "exit") == 0) {
-      printf("Exiting Shell...\n");
-      break;
-   }
+      printf(ANSI_COLOR_BLUE "Exiting Shell...\n");
+      return 0;
+    }
 
     child = fork();
 
     if (child != 0)
       waitpid(-1, &status, 0);
 
-   else {
+   else { 
+      /* There's no binary file for cd
+       * however there's a function that allows the change of the directory
+       * nevertheless, when exit, it goes back to the previous directory
+       * rather than stopping the program */
 
       if (strcmp(command, "cd") == 0){
         chdir(arg);
         continue;
       }
 
-      getcomm(command);
+      Get_Command_Path(command);
       FILE *fd = fopen("dir.txt", "r");
       fgets(path, 100, fd);
       fclose(fd);
       remove("dir.txt");
-      commpath = strtok(path, "\n");
+      commpath = strtok(path, "\n"); // remove \n
+      // Execute the command
       execl(commpath, command, arg, NULL);
     }
   }
+  // Kill the process if not stopped
   kill(child, SIGKILL);
 }
